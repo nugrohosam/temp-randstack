e3f396ad80af19556ec552f751551749626f9913
Ogi Azhar <ogi.azhar3012@gmail.com>
2022-06-28 12:12:55 +0700

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Landing App',
+    titleTemplate: 'BNIL - Landing App',
     title: 'Landing App',
     meta: [{
         charset: 'utf-8'
