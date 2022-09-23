f2c863f2f79836dff0931c874974e67febd3c870
Imanullah <pengguna21@gmail.com>
2022-09-23 23:08:15 +0700

    fix | Tabel 'Data Ahli waris' pada info polis belum vertical

--- pages/polis/information/heirs.vue
+++ pages/polis/information/heirs.vue
@@ -3,43 +3,63 @@
     <!-- Search -->
     <div class="col-12">
       <div class="page-body">
-        <h4>Data {{myPolicy.policyWithCode.isSyariah ? $syariGeneralLabel('Ahli Waris') : "Ahli Waris"}} </h4>
+        <h4>
+          Data
+          {{
+            myPolicy.policyWithCode.isSyariah
+              ? $syariGeneralLabel("Ahli Waris")
+              : "Ahli Waris"
+          }}
+        </h4>
         <br />
         <div class="row">
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th class="text-left">Nama</th>
-                  <th class="text-left">Persentase</th>
-                  <th class="text-left">Jenis Kelamin</th>
-                  <th class="text-left">Hubungan dengan Tertanggung</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  v-for="item in myPolicy.policyWithCode.beneficiaries"
-                  :key="item.name"
-                >
-                  <td>
-                    {{ $isNullWithSpace(item.person.firstName) + $isNullWithSpace(item.person.midName) + $isNullWithSpace(item.person.lastName) }}
-                  </td>
-                  <td>{{ item.shareRate * 100 }}%</td>
-                  <td>{{ item.person.gender == "F" ? "PEREMPUAN" : "LAKI LAKI" }}</td>
-                  <td>{{ $labelRelationType(item.designation) }}</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+          <div class="col-12">
+            <v-data-table
+              :headers="table.headers"
+              mobile-breakpoint="480"
+              hide-default-footer
+              :items="myPolicy.policyWithCode.beneficiaries"
+            >
+              <template v-slot:item.name="{ item }">
+                {{
+                  $isNullWithSpace(item.person.firstName) +
+                  $isNullWithSpace(item.person.midName) +
+                  $isNullWithSpace(item.person.lastName)
+                }}
+              </template>
+              <template v-slot:item.shareRate="{ item }">
+                {{ item.shareRate * 100 }}%
+              </template>
+              <template v-slot:item.gender="{ item }">
+                {{ item.person.gender == "F" ? "PEREMPUAN" : "LAKI LAKI" }}
+              </template>
+              <template v-slot:item.designation="{ item }">
+                {{ $labelRelationType(item.designation) }}
+              </template>
+            </v-data-table>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
+  data() {
+    return {
+      table: {
+        headers: [
+          { text: "Nama", value: "name" },
+          { text: "Persentase", value: "shareRate" },
+          { text: "Jenis Kelamin", value: "gender" },
+          { text: "Hubungan dengan Tertanggung", value: "designation" },
+        ],
+      },
+    };
+  },
   computed: {
-    myPolicy(){
+    myPolicy() {
       return this.$store.getters["submission_transaction/getMyPolicy"];
     },
   },
