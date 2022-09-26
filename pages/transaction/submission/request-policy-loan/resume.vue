09f127c10e1fde6f1b5ce68d937eea1c2a73a4a1
nugrohosam <nugrohosam@macbookpro.local>
2022-09-26 14:02:31 +0700

    Fix Service | Up to dev

--- pages/transaction/submission/request-policy-loan/resume.vue
+++ pages/transaction/submission/request-policy-loan/resume.vue
@@ -86,6 +86,21 @@
     </div>
 
     <div class="row">
+      <div class="col-lg-6 col-sm-12 d-flex">
+        <v-checkbox
+          v-model="accepted"
+          color="orange darken-3"
+          value="orange darken-3"
+          hide-details
+        ></v-checkbox>
+        <p>
+          Saya menyetujui transaksi dan kebenaran data yang disampaikan.
+          <a class="bni-primary no-border" href="">Baca selengkapnya</a>
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
       <div class="col-lg-12 col-sm-12">
         <div class="message-bar rounded-lg">
           <p><b>Perhatian !</b></p>
