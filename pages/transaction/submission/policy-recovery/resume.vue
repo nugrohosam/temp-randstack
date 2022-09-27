ffdb0c88d69fe809a30175e2484a66ef313c6681
nugrohosam <nugrohosam@macbookpro.local>
2022-09-27 19:03:18 +0700

    Fix Service | Pointing to service

--- pages/transaction/submission/policy-recovery/resume.vue
+++ pages/transaction/submission/policy-recovery/resume.vue
@@ -114,23 +114,6 @@
     </div>
 
     <div class="row">
-      <div class="col-lg-6 col-sm-12 d-flex">
-        <v-checkbox
-          v-model="accpeted3"
-          color="orange darken-3"
-          value="orange darken-3"
-          hide-details
-        ></v-checkbox>
-        <p>
-          Saya <b>pemegang polis</b> telah mengisi
-          <b>Health questionnaire</b> dengan benar dan tanpa paksaan, apabila
-          dikemudian hari ditemukan kesalahan pengisian maka saya siap
-          menanggung segala risiko dan akibatnya
-        </p>
-      </div>
-    </div>
-
-    <div class="row">
       <div class="col-lg-12 col-sm-12">
         <div class="message-bar rounded-lg">
           <p><b>Perhatian !</b></p>
@@ -184,7 +167,6 @@ export default {
     return {
       accepted1: false,
       accepted2: false,
-      accepted3: false,
       validationMessage: [],
       showModalHealth: false,
       image_preview: {
@@ -240,7 +222,7 @@ export default {
     },
     validate: async function () {
       this.validationMessage = [];
-      if (!this.accepted1 || !this.accepted2 || !this.accepted3) {
+      if (!this.accepted1 || !this.accepted2) {
         this.validationMessage.push("Setujui semua pernyataan transaksi untuk memproses pengajuan");
       }
     },
